d8f78a6ae1a9fe65400ef6a28aaf038d9cc1a60c
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-02-13 11:11:46 +0100

    Add files via upload

---
+++ markoGalavićProvjeraZnanjaZadatak3/markoGalavićProvjeraZnanjaZadatak3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićProvjeraZnanjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unosTeksta;
+
+            Console.WriteLine("Unesi tekst");
+
+            unosTeksta = Console.ReadLine();
+
+            for (int i = unosTeksta.Length; i > 0; i--)
+            {
+                Console.WriteLine(unosTeksta.Substring(0, i));
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
